67aa25947b77d856ecb2931ec88ad6caea4dd33a
Mark Groves <mark@markgroves.us>
2016-11-26 14:24:30 -0800

    added lib back

---
+++ src/lib/todoist/managers/live_notifications.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from .generic import Manager, GetByIdMixin, AllMixin, SyncMixin
+
+
+class LiveNotificationsManager(Manager, GetByIdMixin, AllMixin, SyncMixin):
+
+    state_name = 'live_notifications'
+    object_type = None  # there is no object type associated
+
+    def set_last_read(self, id):
+        """
+        Sets in the local state the last notification read.
+        """
+        cmd = {
+            'type': 'live_notifications_set_last_read',
+            'uuid': self.api.generate_uuid(),
+            'args': {
+                'id': id,
+            },
+        }
+        self.queue.append(cmd)
